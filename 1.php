4b52a1c94a297a5464cedfe5a469a8f7b7c1b719
lensqueen <addatir99@gmail.com>
2020-06-06 14:26:05 +0530

    It is Photography Website

---
+++ 1.php
@@ -0,0 +1,5 @@
+<?php
+$num=$_POST["name"];
+echo"<br><h1><center>Thanks &nbsp";
+echo "<br><h1>Data Successful</center>";
+?>
---
+++ seventeen.php
@@ -0,0 +1,5 @@
+<?php
+$num=$_POST["email"];
+echo"<br><h1><center>Thanks &nbsp";
+echo "<br><h1>Login Successfully</center>";
+?>
